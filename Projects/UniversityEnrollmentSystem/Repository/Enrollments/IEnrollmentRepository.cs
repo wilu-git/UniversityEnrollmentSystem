8cf5aeb38b3c9563d9e798cb916287f5cc93cc35
XunilTamtacles <bbondocmattlinux@gmail.com>
2026-02-19 21:02:03 +0800

    added

--- Projects/UniversityEnrollmentSystem/Repository/Enrollments/IEnrollmentRepository.cs
+++ Projects/UniversityEnrollmentSystem/Repository/Enrollments/IEnrollmentRepository.cs
@@ -1,6 +1,32 @@
-﻿namespace UniversityEnrollmentSystem.Repository.Enrollments
+﻿using UniversityEnrollmentSystem.Models;
+
+namespace UniversityEnrollmentSystem.Repository.Enrollments
 {
     public class IEnrollmentRepository
     {
+        internal async Task AddAsync(Enrollment enrollment)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task<bool> ExistsAsync(int studentId, int courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task GetByIdAsync(int enrollmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task GetCourseWithEnrollmentsAsync(int courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal async Task UpdateAsync(object enrollment)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
